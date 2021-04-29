f78f3599e3d9eeed465207fac8d9843d64fc0dc4
kazuya002ex <chiharu.event2@gmail.com>
2021-04-29 14:54:02 +0900

    feat: アプリを作成

--- helloworldproject/urls.py
+++ helloworldproject/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from .views import hellorworldfunc
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('helloworldurl/', hellorworldfunc),
-    path('helloworldurl2/', HelloWorldClass.as_view()),
+    path('', include('helloworldapp.urls')),
 ]
